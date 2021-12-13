b7f98fb47b4fcbe3a6412c361f2c928ea82202d6
Meapy <dkrasovskijj@gmail.com>
2021-12-13 22:39:18 +0000

    camera now mostly working

--- Assets/Camera.cs
+++ Assets/Camera.cs
@@ -16,14 +16,23 @@ public class Camera : MonoBehaviour
     {
         if (target)
         {
-            transform.position = Vector3.Lerp(transform.position, target.position+target_Offset, 1.1f);
+            if(target.transform.eulerAngles.y > 100 && target.transform.eulerAngles.y < 180 || target.transform.eulerAngles.y <= -180 && target.transform.eulerAngles.y >= -100)
+            {
+                //transform.position = new Vector3(transform.position.x, transform.position.y, target.transform.position.z + 30);
+                float zAxis = target.transform.position.z + 30;
+                transform.position = Vector3.Lerp(transform.position, target.position-target_Offset,1.1f);
+                transform.eulerAngles = new Vector3(transform.eulerAngles.x, 180, transform.eulerAngles.z);
+                Debug.Log("#2 is called");
+            }
+            else
+            {
+                transform.position = Vector3.Lerp(transform.position, target.position+target_Offset, 1.1f);
+                transform.eulerAngles = new Vector3(transform.eulerAngles.x, 0, transform.eulerAngles.z);
+                Debug.Log("#1 is called");
+            }
+
         }
         RotateCamera();
-        // if c is pressed, update camera position
-        if (Input.GetKeyDown(KeyCode.C))
-        {
-            UpdateCamera();
-        }
     }
     void RotateCamera()
     {
@@ -33,23 +42,5 @@ public class Camera : MonoBehaviour
 
             transform.RotateAround(transform.position, transform.right,-Input.GetAxis("Mouse Y")*speed);
         }
-    }
-    void UpdateCamera()
-    {
-        // if the target rotation is between -140 to -180 or 140 to 180 then change the camera position z axis to be +20 else -20
-        if(target.transform.eulerAngles.y > 140 && target.transform.eulerAngles.y < 180 || target.transform.eulerAngles.y > -180 && target.transform.eulerAngles.y < -140)
-        {
-            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + 30);
-            //change the camera rotation y axis to be -180
-            transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y + 180, transform.eulerAngles.z);
-            Debug.Log("#1 is called");
-        }
-        else
-        {
-            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - 30);
-            transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y -180, transform.eulerAngles.z);
-            Debug.Log("#2 is called");
-
-        }  
     }
 }
